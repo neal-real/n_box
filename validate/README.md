12939231efbdfdf7dbc7b6d30ee7920fec474b75
neal <real_is_best@163.com>
2021-09-12 16:02:46 +0800

    调整数据校验模块
    调整数据校验模块

--- validate/README.md
+++ validate/README.md
@@ -1,14 +1,310 @@
-## 数据校验模块
+# 数据校验模块
 
+## 说明
 
+- 本模块是 GitHub 上  `parameter` 的中文学抄版本， 是在学习 `eggjs` 了解到数据校验的 `egg`版本，然后其中讲到使用了 `parameter` 但是因为种种原因都不符合本人的使用习惯，然后就重新整理给予自己使用。在自己使用的过程中也秉承前人栽树后人乘凉的想法分享出来。
+- 好用你笑纳，无用你见笑。
+- 本人不对后续更新，持续维护，使用后果做任何形式的承诺。
 
-## 使用前提
-
-#### 依赖
-
+## 安装
 ```shell
-npm i parameter
+$ npm install neal-parameter --save
 ```
+
+#### 
+
+## 用法
+
+### API
+
+`Parameter` Class
+
+- `constructor([options])` - new Class `Parameter` instance
+  - `options.translate` - translate function
+  - `options.validateRoot` - config whether to validate the passed in value must be a object, default to `false`.
+  - `options.convert` - convert primitive params to specific type, default to `false`.
+  - `options.widelyUndefined` - convert empty string(`''`), NaN, Null to undefined, this option can make `rule.required` more powerful, default to `false`.__This may change the original input params__.
+- `validate(rule, value)` - validate the `value` conforms to `rule`. return an array of errors if break rule.
+- `addRule(type, check)` - add custom rules.
+  - `type` - rule type, required and must be string type.
+  - `check` - check handler. can be a `function` or a `RegExp`.
+
+
+
+__Note:当”选项。convert ' enabled，所有内置规则检查原始输入参数并将其转换为规则的默认' convertType '(定义如下)，你也可以在每个规则定义中通过' convertType '选项为特定规则启用该特性
+
+
+
+### 例子
+
+```js
+var Parameter = require('parameter');
+
+var parameter = new Parameter({
+  translate: function() {
+    var args = Array.prototype.slice.call(arguments);
+    // Assume there have I18n.t method for convert language.
+    return I18n.t.apply(I18n, args);
+  },
+  validateRoot: true, // restrict the being validate value must be a object
+});
+
+var data = {
+  name: 'foo',
+  age: 24,
+  gender: 'male'
+};
+
+var rule = {
+  name: 'string',
+  age: 'int',
+  gender: ['male', 'female', 'unknown']
+};
+
+var errors = parameter.validate(rule, data);
+```
+
+#### [复杂例子](example.js)
+
+
+
+### 规则
+
+#### 通用规则
+
+- `type` - 属性的类型，每种类型都有自己的验证规则.
+- `convertType` - 将输入参数转换为特定类型，支持`int`、`number`、`string`和`boolean`，还支持自定义转换方法的函数
+- `required` - 必填项，默认是 true。如果某个字段是可选项需要主动设置为：false
+- `default` - 属性的默认值，如果该属性被手动设置为：`required: false` 此参数的值，将作为默认值。
+  - 这可能会更改原始输入参数。
+- `widelyUndefined` - 覆盖 选项 `options.widelyUndefined`
+
+__注意：您可以将 require 和 type 以符号`?` 结尾，例如：`int?` 或 `string?` 来指定类型和非必需。__
+
+
+
+### type：可选值
+
+#### int
+
+type 是 `int`，有两个可选规则
+
+- `max` - 最大值: 值必须小于或等于 `max` 设置的值,  `value`  <= `max`.
+- `min` - 最小值: 值必须大于或等于 `min` 设置的值, , `value`  >= `min`.
+
+默认 `convertType` 是 `int`
+
+__注意：默认的 `convertType` 只会在 `options.convert` 在参数的构造函数中设置为 true 时起作用。__
+
+#### integer
+
+ `int`的别名
+
+#### number
+
+type 是 `number`，有两个可选规则， 同`int`
+
+- `max` - 最大值: 值必须小于或等于 `max` 设置的值,  `value`  <= `max`.
+- `min` - 最小值: 值必须大于或等于 `min` 设置的值, , `value`  >= `min`.
+
+默认 `convertType` 是 `number`.
+
+#### date
+
+`date` 类型要匹配 `YYYY-MM-DD` 类型的日期字符串
+
+默认 `convertType` 是 `string`。
+
+#### dateTime
+
+`dateTime` 类型要匹配 `YYYY-MM-DD HH:mm:ss` 类型的日期字符串。
+
+默认 `convertType` 是 `string`。
+
+#### datetime
+
+ `dateTime`的别名
+
+#### id
+
+'id'类型要与`/^\d+$/`类型日期字符串匹配。
+
+默认 `convertType` 是 `string`。
+
+#### boolean
+
+匹配 `boolean` 类型值。
+
+默认 `convertType` 是 `boolean`。
+
+#### bool
+
+ `boolean`的别名
+
+#### string
+
+类型为“string”，有四条可选规则
+
+- `allowEmpty`(alias to `empty`) - 允许空字符串，默认为false。 如果 `required` 设置为 false，默认情况下 `allowEmpty` 将设置为 `true`。
+- `format` -用于检查字符串格式的`RegExp`（正则表达式）. 
+- `max` - 字符串的最大长度
+- `min` - 字符串的最小长度
+- `trim` - 检查前修剪字符串前后的空格，默认为`false`。
+
+默认 `convertType` 是 `string`。
+
+#### email
+
+要匹配的`email`类型[[RFC 5322]](https://datatracker.ietf.org/doc/html/rfc5322#section-3.4) 电子邮件地址。
+
+- `allowEmpty` -允许空字符串，默认为false。
+
+默认 `convertType` 是 `string`。
+
+#### password
+
+`password` 类型要匹配 `/^$/` 类型的字符串
+
+- `compare` - Compare field to check equal, default null, not check.
+- `max` - 密码的最大长度
+- `min` - 密码的最小长度，默认为6
+
+默认 `convertType` 是 `string`。
+
+#### url
+
+`url` 类型要匹配 [web url](https://gist.github.com/dperini/729294).
+
+默认 `convertType` 是 `string`。
+
+#### enum
+
+type为' enum '，则需要一个附加规则
+
+- `values` - 值是数组，' value '必须为其中之一。此规则是必填项
+
+#### object
+
+type 是 `object`，则有一个可选规则：
+
+- `rule` - 验证对象属性的对象
+
+#### array
+
+type 是 `array`，则有四个可选规则
+
+- `itemType` - 可声明数组中每个 item 的类型
+- `rule` - 验证数组项的对象。 仅适用于`itemType`
+- `max` - 数组的最大长度
+- `min` - 数组的最小长度
+
+#### 简写的
+
+> 示例：type:int
+
+- `'int'` => `{type: 'int', required: true}`
+- `'int?'` => `{type: 'int', required: false }`
+- `'integer'` => `{type: 'integer', required: true}`
+- `'number'` => `{type: 'number', required: true}`
+- `'date'` => `{type: 'date', required: true}`
+- `'dateTime'` => `{type: 'dateTime', required: true}`
+- `'id'` => `{type: 'id', required: true}`
+- `'boolean'` => `{type: 'boolean', required: true}`
+- `'bool'` => `{type: 'bool', required: true}`
+- `'string'` => `{type: 'string', required: true, allowEmpty: false}`
+- `'string?'` => `{type: 'string', required: false, allowEmpty: true}`
+- `'email'` => `{type: 'email', required: true, allowEmpty: false, format: EMAIL_RE}`
+- `'password'` => `{type: 'password', required: true, allowEmpty: false, format: PASSWORD_RE, min: 6}`
+- `'object'` => `{type: 'object', required: true}`
+- `'array'` => `{type: 'array', required: true}`
+- `[1, 2]` => `{type: 'enum', values: [1, 2]}`
+- `/\d+/` => `{type: 'string', required: true, allowEmpty: false, format: /\d+/}`
+
+
+
+
+
+
+
+### `错误` 示例
+
+#### `code: 遗漏字段`
+
+```js
+{
+  code: 'missing_field',
+  field: 'name',
+  message: 'required'
+}
+```
+
+#### `code: invalid`
+
+```js
+{
+  code: 'invalid',
+  field: 'age',
+  message: 'should be an integer'
+}
+```
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #### 提供校验文件
 
---
+++ validate/临时/parameter-master/benchmark.js
@@ -0,0 +1,125 @@
+'use strict';
+
+var Benchmark = require('benchmark');
+var benchmarks = require('beautify-benchmark');
+var Parameter = require('./');
+
+var suite = new Benchmark.Suite();
+
+var data = {
+  id: '043624',
+  nick: '苏千',
+  date: '2013-06-25',
+  time: '2013-06-25 01:12:22',
+  age: 29,
+  sid1: 123,
+  sid2: '123',
+  sid3: 'foo',
+  unit: 'y',
+  email: 'fengmk2@gmail.com',
+  password: '!@#123',
+  're-password': '!@#123',
+  url: 'https://github.com/node-modules/parameter/compare/url?expand=1'
+};
+
+var rules = [
+  { id: 'id' },
+  { id: { type: 'id' } },
+
+  { date: 'date' },
+  { date: { type: 'date' } },
+
+  { time: 'datetime' },
+  { time: { type: 'datetime' } },
+
+  { age: 'number' },
+  { age: { type: 'number' } },
+
+  { nick: 'string'},
+  { nick: { type: 'string' } },
+  { not_exists: 'string', required: false },
+  { sid1: /^\d+$/},
+  { sid2: { type: 'string', format: /^\d+$/ } },
+  // { sid3: { type: 'string', format: /^\d+$/, message: 'should be digital' } },
+
+  { unit: ['y', 'm', 'd', 'w'] },
+  { unit: { type: 'enum', values: ['y', 'm', 'd', 'w'] } },
+  { unit: ['yy', 'mm', 'dd', 'ww'] },
+
+  { email: 'email' },
+
+  { password: { type: 'password', compare: 're-password' } },
+
+  { url: 'url'},
+];
+
+function json(obj) {
+  return JSON.stringify(obj);
+}
+
+var globalParameter = new Parameter();
+
+rules.forEach(function (rule) {
+  suite.add('globalParameter: verify ' + json(rule), function () {
+    globalParameter.validate(rule, data);
+  });
+
+  suite.add('localParameter: verify ' + json(rule), function () {
+    var p = new Parameter();
+    p.validate(rule, data);
+  });
+});
+
+suite.on('cycle', function(event) {
+  benchmarks.add(event.target);
+})
+.on('start', function(event) {
+  console.log('\n  node version: %s, date: %s\n  Starting...', process.version, Date());
+})
+.on('complete', function done() {
+  benchmarks.log();
+})
+.run({ 'async': false });
+
+// node version: v2.5.0, date: Fri Sep 25 2015 23:29:32 GMT+0800 (CST)
+// Starting...
+// 38 tests completed.
+//
+// globalParameter: verify {"id":"id"}                                              x 1,755,826 ops/sec ±1.14% (92 runs sampled)
+// localParameter: verify {"id":"id"}                                               x 1,700,584 ops/sec ±1.11% (91 runs sampled)
+// globalParameter: verify {"id":{"type":"id"}}                                     x 1,664,283 ops/sec ±1.26% (90 runs sampled)
+// localParameter: verify {"id":{"type":"id"}}                                      x 1,654,914 ops/sec ±1.35% (91 runs sampled)
+// globalParameter: verify {"date":"date"}                                          x 1,547,474 ops/sec ±1.22% (92 runs sampled)
+// localParameter: verify {"date":"date"}                                           x 1,543,998 ops/sec ±1.00% (92 runs sampled)
+// globalParameter: verify {"date":{"type":"date"}}                                 x 1,528,876 ops/sec ±1.17% (91 runs sampled)
+// localParameter: verify {"date":{"type":"date"}}                                  x 1,531,417 ops/sec ±1.16% (93 runs sampled)
+// globalParameter: verify {"time":"datetime"}                                      x 1,436,431 ops/sec ±1.16% (91 runs sampled)
+// localParameter: verify {"time":"datetime"}                                       x 1,435,370 ops/sec ±1.14% (91 runs sampled)
+// globalParameter: verify {"time":{"type":"datetime"}}                             x 1,412,463 ops/sec ±1.30% (92 runs sampled)
+// localParameter: verify {"time":{"type":"datetime"}}                              x 1,399,845 ops/sec ±1.74% (91 runs sampled)
+// globalParameter: verify {"age":"number"}                                         x 2,708,155 ops/sec ±1.27% (91 runs sampled)
+// localParameter: verify {"age":"number"}                                          x 2,587,876 ops/sec ±1.33% (92 runs sampled)
+// globalParameter: verify {"age":{"type":"number"}}                                x 2,607,889 ops/sec ±1.10% (93 runs sampled)
+// localParameter: verify {"age":{"type":"number"}}                                 x 2,538,770 ops/sec ±1.31% (91 runs sampled)
+// globalParameter: verify {"nick":"string"}                                        x 2,307,909 ops/sec ±1.13% (94 runs sampled)
+// localParameter: verify {"nick":"string"}                                         x 2,206,572 ops/sec ±1.27% (92 runs sampled)
+// globalParameter: verify {"nick":{"type":"string"}}                               x 2,296,899 ops/sec ±0.98% (94 runs sampled)
+// localParameter: verify {"nick":{"type":"string"}}                                x 2,170,179 ops/sec ±1.52% (92 runs sampled)
+// globalParameter: verify {"not_exists":"string","required":false}                 x   160,931 ops/sec ±1.15% (91 runs sampled)
+// localParameter: verify {"not_exists":"string","required":false}                  x   158,010 ops/sec ±1.63% (93 runs sampled)
+// globalParameter: verify {"sid1":{}}                                              x   266,353 ops/sec ±3.10% (87 runs sampled)
+// localParameter: verify {"sid1":{}}                                               x   278,138 ops/sec ±1.61% (90 runs sampled)
+// globalParameter: verify {"sid2":{"type":"string","format":{}}}                   x 1,711,115 ops/sec ±1.15% (90 runs sampled)
+// localParameter: verify {"sid2":{"type":"string","format":{}}}                    x 1,712,745 ops/sec ±1.07% (95 runs sampled)
+// globalParameter: verify {"unit":["y","m","d","w"]}                               x 2,763,750 ops/sec ±1.26% (90 runs sampled)
+// localParameter: verify {"unit":["y","m","d","w"]}                                x 2,703,971 ops/sec ±1.17% (93 runs sampled)
+// globalParameter: verify {"unit":{"type":"enum","values":["y","m","d","w"]}}      x 3,930,004 ops/sec ±1.43% (91 runs sampled)
+// localParameter: verify {"unit":{"type":"enum","values":["y","m","d","w"]}}       x 3,839,268 ops/sec ±1.40% (92 runs sampled)
+// globalParameter: verify {"unit":["yy","mm","dd","ww"]}                           x   186,797 ops/sec ±1.75% (92 runs sampled)
+// localParameter: verify {"unit":["yy","mm","dd","ww"]}                            x   188,961 ops/sec ±1.30% (94 runs sampled)
+// globalParameter: verify {"email":"email"}                                        x   370,479 ops/sec ±1.92% (89 runs sampled)
+// localParameter: verify {"email":"email"}                                         x   376,473 ops/sec ±1.19% (90 runs sampled)
+// globalParameter: verify {"password":{"type":"password","compare":"re-password"}} x 1,252,958 ops/sec ±1.12% (91 runs sampled)
+// localParameter: verify {"password":{"type":"password","compare":"re-password"}}  x 1,232,913 ops/sec ±1.47% (90 runs sampled)
+// globalParameter: verify {"url":"url"}                                            x   269,519 ops/sec ±1.15% (93 runs sampled)
+// localParameter: verify {"url":"url"}                                             x   262,017 ops/sec ±1.23% (91 runs sampled)
